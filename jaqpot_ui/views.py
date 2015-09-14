692c8c50191204b73916e1df4708344e045136bb
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2015-09-14 16:36:36 +0300

    Change pagination of choose dataset template. Change structure of dataset.

--- jaqpot_ui/views.py
+++ jaqpot_ui/views.py
@@ -354,25 +354,29 @@ def trainmodel(request):
     if request.method == 'GET':
         dataset=[]
         headers = {'Accept': 'application/json', 'subjectid': token}
+        #get total number of datasets
+        headers1 = {'Accept': 'text/plain', 'subjectid': token}
+        res1= requests.get(SERVER_URL+'/dataset/count?creator='+username, headers=headers1)
+        total_datasets= int(res1.text)
+        if total_datasets%20 == 0:
+            last = total_datasets/20
+        else:
+            last = (total_datasets/20)+1
 
         if page:
+            #page1 is the number of first dataset of page
             page1=int(page) * 20 - 20
             k=str(page1)
             if page1 <= 1:
-                res = requests.get(SERVER_URL+'/dataset?start=0&max=20', headers=headers)
-            elif last:
-                res = requests.get(SERVER_URL+'/dataset?start='+last+'&max=20', headers=headers)
+                res = requests.get(SERVER_URL+'/dataset?creator='+username+'&start=0&max=20', headers=headers)
             else:
-                res = requests.get(SERVER_URL+'/dataset?start='+k+'&max=20', headers=headers)
-
+                res = requests.get(SERVER_URL+'/dataset?creator='+username+'&start='+k+'&max=20', headers=headers)
         else:
             page = 1
-            res = requests.get(SERVER_URL+'/dataset?start=0&max=20', headers=headers)
+            res = requests.get(SERVER_URL+'/dataset?creator='+username+'&start=0&max=20', headers=headers)
         data= json.loads(res.text)
         for d in data:
             dataset.append({'name': d['_id'], 'meta': d['meta']})
-        if len(dataset)< 20:
-            last= page
 
         return render(request, "choose_dataset.html", {'token': token, 'username': username, 'entries2': dataset, 'page': page, 'last':last})
 
@@ -458,13 +462,16 @@ def change_params(request):
         res2 = requests.get(SERVER_URL+'/dataset/'+dataset+'?rowStart=0&rowMax=1&colStart=0&colMax=2')
         predicted_features = json.loads(res2.text)
         print res2.text
-        if predicted_features['httpStatus'] == 200:
+        print res2
+        if str(res2) != "<Response [200]>":
+            #redirect to error page
+            return render(request, "error.html", {'token': token, 'username': username,'error':predicted_features})
+        else:
             features = predicted_features['features']
             print features
             return render(request, "alg.html", {'token': token, 'username': username, 'dataset':dataset, 'al': al, 'algorithms':algorithms, 'pmml': pmml_id, 'uploadform':form, 'features':features})
-        else:
-            #redirect to error page
-            return render(request, "error.html", {'token': token, 'username': username,'error':predicted_features})
+
+
     if request.method == 'POST':
         #get parameters of algorithm
         params=[]
