6aa05ffbdcfc0db4701cd3f6616f73576811e54b
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2015-10-08 12:47:46 +0300

    Add tooltip with units and conditions to the headers of the excel sheet.

---
+++ jaqpot_ui/create_dataset.py
@@ -0,0 +1,64 @@
+__author__ = 'evangelie'
+
+
+def create_dataset( data, username, required_res ):
+   #This function creates json dataset
+
+   #replace name with uri
+    new_data=[]
+
+    for d in data:
+        n_d={}
+        n_d1={}
+        for key, value in d.items():
+            for r in required_res:
+                if r['name'] == key:
+                    n=r['uri']
+                    n_d1[''+n+'']=value
+                    n_d.update(n_d1)
+        new_data.append(n_d)
+    data1 = {}
+    data2 = {}
+    data3 = {}
+    data4 = {}
+    data5 = []
+    data6 = {}
+    data7 = {}
+    data8 = {}
+    data9 = {}
+    data10 = {}
+    data11= {}
+
+    data1['comments'] = [""]
+    data1['descriptions'] = [""]
+    data1['titles'] = ["new dataset"]
+    data1['creators'] = [username]
+    data1['hasSources'] = [""]
+    data2['meta'] = data1
+
+    counter=1
+    for d in new_data:
+        data3["values"] = d
+        data4["name"] = 'compound'+str(counter)
+        data4["URI"] = 'compound'+str(counter)
+        data3["compound"] = data4
+        data5.append(data3)
+        totalColumns= len(d)
+        counter = counter+1
+        data3 = {}
+        data4 = {}
+
+    data8["dataEntry"]= data5
+
+    data6["descriptors"] = [ "EXPERIMENTAL" ]
+    data9["totalColumns"] = totalColumns
+    data10["totalRows"] = len(new_data)
+    data11["features"] = required_res
+
+    data7.update(data2)
+    data7.update(data8)
+    data7.update(data6)
+    data7.update(data9)
+    data7.update(data10)
+    data7.update(data11)
+    return data7
--- jaqpot_ui/views.py
+++ jaqpot_ui/views.py
@@ -4,6 +4,7 @@ from urllib import urlencode
 from django.shortcuts import render, redirect, render_to_response
 from django.template import RequestContext
 from elasticsearch import Elasticsearch
+from jaqpot_ui.create_dataset import create_dataset
 from jaqpot_ui.forms import UserForm, BibtexForm, TrainForm, FeatureForm, ContactForm, SubstanceownerForm, UploadFileForm, \
     TrainingForm, InputForm, NoPmmlForm, SelectPmmlForm
 import requests
@@ -976,68 +977,10 @@ def predict_model(request):
             if 'excel_data' in request.POST:
                 data = request.POST.get('excel_data')
                 data = json.loads(data)
-                #replace name with uri
-                new_data=[]
+                #Get data from excel and create dataset to the appropriate format
+                new_data = create_dataset(data,username,required_res)
 
-                for d in data:
-                    n_d={}
-                    n_d1={}
-                    for key, value in d.items():
-                        for r in required_res:
-                            if r['name'] == key:
-                                n=r['uri']
-                                n_d1[''+n+'']=value
-                                n_d.update(n_d1)
-                    new_data.append(n_d)
-                print new_data
-
-                #Create dataset json
-                data1 = {}
-                data2 = {}
-                data3 = {}
-                data4 = {}
-                data5 = []
-                data6 = {}
-                data7 = {}
-                data8 = {}
-                data9 = {}
-                data10 = {}
-                data11= {}
-
-
-                data1['comments'] = [""]
-                data1['descriptions'] = [""]
-                data1['titles'] = ["new dataset"]
-                data1['creators'] = [username]
-                data1['hasSources'] = [""]
-                data2['meta'] = data1
-
-                counter=1
-                for d in new_data:
-                    data3["values"] = d
-                    data4["name"] = 'compound'+str(counter)
-                    data3["compound"] = data4
-                    data5.append(data3)
-                    totalColumns= len(d)
-                    counter = counter+1
-                    data3 = {}
-                    data4 = {}
-
-                data8["dataEntry"]= data5
-
-                data6["descriptors"] = [ "EXPERIMENTAL" ]
-                data9["totalColumns"] = totalColumns
-                data10["totalRows"] = len(new_data)
-                data11["features"] = required_res
-
-                data7.update(data2)
-                data7.update(data8)
-                data7.update(data6)
-                data7.update(data9)
-                data7.update(data10)
-                data7.update(data11)
-
-                json_data = json.dumps(data7)
+                json_data = json.dumps(new_data)
                 headers1 = {'Content-type': 'application/json', 'subjectid': token}
                 res = requests.post(SERVER_URL+'/dataset', headers=headers1, data=json_data)
                 print res.text
